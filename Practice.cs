bbf2baf83f196b94547dd7efca5c163f7e8218d0
Rushillachmon <125268172+Rushillachmon@users.noreply.github.com>
2023-04-25 13:16:35 +0200

    random change

--- Practice.cs
+++ Practice.cs
@@ -1,2 +1,6 @@
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! my name is Rushil");
+Console.WriteLine("Hello, World! my name is Rushil and my name is Ceylan");
+
+
+
+
